59b4742574d95c6cebd1113b14b70e417c3fac8c
Your Name <you@example.com>
2025-07-24 18:11:32 -0700

    Finish manus_agent and open_interpreter scaffolding

---
+++ open_interpreter/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+\"\"\"Aurelius manus_agent entrypoint\"\"\"
+
+from openai import OpenAI
+
+def main():
+    client = OpenAI()
+    # TODO: wire up your prompts and task automation
+
+if __name__ == "__main__":
+    main()
